5f0b3b04b92929384fb68149c77b7c7edaef4d7e
Arsen <anosovarsenij@gmail.com>
2025-06-16 11:04:31 +0200

    calculator

---
+++ calculator.ts
@@ -0,0 +1,23 @@
+import * as readline from 'readline';
+
+function askQuestion(query: string) : Promise<string>{
+    const rl = readline.createInterface({
+        input:process.stdin,
+        output:process.stdout
+    })
+
+    return new Promise(resolve => rl.question(query, answer => {
+        rl.close();
+        resolve(answer);
+    }))
+}
+
+async function calculator(){
+    let number1:number = Number( await askQuestion("Enter the first number: "));
+    let number2:number = Number( await askQuestion("Enter the second number: "));
+    console.log(`Result = ${number1+number2}`)
+}
+
+calculator();
+
+
